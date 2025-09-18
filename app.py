a4c60582223fdf2eaa49faac1d3620ce2635b4a4
mateushildebrando <146665587+mateushildebrando@users.noreply.github.com>
2025-09-18 08:09:28 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from werqzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ db_config = {
     'database' : 'pg2'
 }
 
-@app.route('/cadastro', methods=['GET', 'POST'])
+@app.route('/cadastro', methods=['GET', 'PHOST'])
 def cadastro():
     if request.method == 'POST':
         nome = request.form['nome']
@@ -20,10 +20,44 @@ def cadastro():
         email = request.form['email']
         senha = generate_password_hash(request.form['senha'])
 
-conn = mysql.connector.connect(**db_config)
-cursor = conn.cursor()
+        conn = mysql.connector(**db_config)
+        cursor = conn.cursor()
 
-cursor.execute ("SELECT * FROM usuario WHERE username_usuario = %s OR email usuario OR email_usuario = %s", (username, email))
-if cursor.fetchone():
-    flash("Nome de usuário ou email já cadastrado.", "erro")
-    return redirect(url_for('cadastro'))
+        cursor.execute("SELECT * FROM usuario   WHERE username_usuario = %s OR email_usuario = %s", (username, email))
+        if cursor.fetchone():
+            flash("Nome de usuario ou email já cadastrado.", "erro")
+            return redirect(url_for('cadastro'))
+
+        cursor.execute (""" INSERT INTO usuario (nome_usuario, username_usuario, password_usuario, email_usuario, conta_ativa) 
+                        VALUES (%s, %s, %s, %s, %s)""", (nome, username, senha, email, True)
+                        )
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        flash("Cadastro realizado com sucesso! Você já pode fazer login.",  "Sucesso" )
+        return redirect(url_for('login'))
+
+
+    return render_template('cadastro.html')
+
+@app.route('login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':    
+        username = request.form['username'].strip()
+        senha = request.form["senha"].strip()
+
+
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute ("SELECT * FROM usuario WHERE username_usuario = "
+        "%s", (username,))
+        usuario = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+
+        if usuario and check_password_hash(usuario['password_usuario'], senha ):
+            if not usuario['conta_livre']:
+                flash("Esta conta está desativada.", "Erro")
+                return redirect(url_for('login'))
